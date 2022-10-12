801d29666379ef7f485a98ebe0a9ebce21ba2e2e
Sponge166 <115596620+Sponge166@users.noreply.github.com>
2022-10-11 20:50:54 -0400

    Update MoreComparators.java
    adding more complex example

--- MoreComparators.java
+++ MoreComparators.java
@@ -11,10 +11,24 @@ public class MoreComparators {
 	public static void main(String args[]) {
 		List<Number> nums = Arrays.asList(1, 3.25, 3.14, 3, 1.5, 12);
 		
+		System.out.println("before sort: " + nums);
+		
 		Collections.sort(nums, comparatorFactory((Number x) -> x.doubleValue()));
 		
-		System.out.println(nums);
+		System.out.println("after sort: " + nums);
 		
+		ArrayList<List<Double>> list2d = new ArrayList<List<Double>>();
+		
+		list2d.add(Arrays.asList(1.2, 6.8, 4.6));
+		list2d.add(Arrays.asList(4.8, 2.1, 8.9));
+		list2d.add(Arrays.asList(2.5, -.1, 2.0));
+		list2d.add(Arrays.asList(0.1, 9.8, 6.7));
+		
+		System.out.println("before sort: " + list2d);
+		
+		Collections.sort(list2d, comparatorFactory( (List<Double> x) -> Collections.max(x) ));
+		
+		System.out.println("after sort: " + list2d);
 	}
 	
 	public static <T, U extends Comparable<? super U>> Comparator<T> comparatorFactory(Function<? super T, ? extends U> func){
